3e6430c96c71e96a9b045b2a340b083b75cef938
Tektor20 <matejbozac1@gmail.com>
2025-04-12 19:02:58 +0200

    Created Message.java for database

---
+++ src/main/java/com/example/rsikspr_projekt/model/Message.java
@@ -0,0 +1,38 @@
+package com.example.rsikspr_projekt.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Schema(description = "A message stored in the database")
+@ToString
+@Getter @Setter
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Message ID", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
+    private long Id;
+
+    @Column(nullable = false, length = 500)
+    @Schema(description = "Message content", requiredMode = Schema.RequiredMode.REQUIRED, example = "healthy:true")
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    @Schema(description = "When was the message recived", accessMode = Schema.AccessMode.READ_ONLY)
+    private LocalDateTime recivedAt;
+
+    Message(){
+    }
+
+    Message(String content, LocalDateTime recivedAt) {
+        this.content = content;
+        this.recivedAt = recivedAt;
+    }
+}
