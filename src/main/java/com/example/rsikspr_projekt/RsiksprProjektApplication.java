7a0148cf5598c87854151609955dcc7cd4407080
Tektor20 <matejbozac1@gmail.com>
2025-04-12 16:59:33 +0200

    Innit commit

---
+++ src/main/java/com/example/rsikspr_projekt/RsiksprProjektApplication.java
@@ -0,0 +1,15 @@
+package com.example.rsikspr_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsiksprProjektApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RsiksprProjektApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
